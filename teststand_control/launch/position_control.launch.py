695d349ef59c7c0511a0d3b194eef602aea67014
Victor T. N <victortn21@hotmail.com>
2024-09-05 20:39:59 +0000

    Make teststand_control completely agnostic (works the same with simulation and hardware)
    Signed-off-by: Victor T. N <victortn21@hotmail.com>

--- teststand_control/launch/position_control.launch.py
+++ teststand_control/launch/position_control.launch.py
@@ -4,6 +4,7 @@ from ament_index_python.packages import get_package_share_directory
 from ament_index_python.packages import get_package_prefix
 
 from launch import LaunchDescription
+from launch.substitutions import EnvironmentVariable
 
 from launch_ros.actions import Node
 
@@ -12,20 +13,46 @@ def generate_launch_description():
     # Position controller
     pos_cont_spawner = Node(
         package="controller_manager",
+        prefix=[
+            "sudo -E env PATH=",
+            EnvironmentVariable("PATH", default_value="${PATH}"),
+            " LD_LIBRARY_PATH=",
+            EnvironmentVariable("LD_LIBRARY_PATH", default_value="${LD_LIBRARY_PATH}"),
+            " PYTHONPATH=",
+            EnvironmentVariable("PYTHONPATH", default_value="${PYTHONPATH}"),
+            " HOME=/tmp ",
+        ],
         executable="spawner",
-        arguments=["position_controller"])
+        arguments=["forward_position_controller",
+                   "--controller-manager",
+                   "/controller_manager",
+        ],
+    )
 
     # Joint broadcaster
     joint_broad_spawner = Node(
         package="controller_manager",
+        prefix=[
+            "sudo -E env PATH=",
+            EnvironmentVariable("PATH", default_value="${PATH}"),
+            " LD_LIBRARY_PATH=",
+            EnvironmentVariable("LD_LIBRARY_PATH", default_value="${LD_LIBRARY_PATH}"),
+            " PYTHONPATH=",
+            EnvironmentVariable("PYTHONPATH", default_value="${PYTHONPATH}"),
+            " HOME=/tmp ",
+        ],
         executable="spawner",
-        arguments=["joint_broadcaster"])
+        arguments=["joint_state_broadcaster",
+                   "--controller-manager",
+                   "/controller_manager",
+        ],
+    )
 
     # Reference signal generator
     ref_signal_generator = Node(
         package="reference_signal_generator",
         executable="reference_signal_generator",
-        ros_arguments=["-p", "topic_name:=/position_controller/commands"])
+        ros_arguments=["-p", "topic_name:=/forward_position_controller/commands"])
 
     # Launch them all!
     return LaunchDescription([
